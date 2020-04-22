8a9f9e0f6f8d59ec622a79873c3e8c974272e68c
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-22 21:16:33 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ActiveRecord::Base
   has_many :tweets
   
   
- 
+  def slug
+    self.username.downcase.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find do |user|
+      user.username.downcase == slug.gsub('-', ' ')
+    end
+  end
   
   
 end
